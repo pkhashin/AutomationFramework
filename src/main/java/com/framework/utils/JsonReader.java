af3f4a9bd83dd8c2c4ab754b592a50455dd8a8ac
hashinpk <pkhashin@gmail.com>
2025-07-29 17:26:08 +0530

    Added the config file

--- src/main/java/com/framework/utils/JsonReader.java
+++ src/main/java/com/framework/utils/JsonReader.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 
 public class JsonReader {
     private static final String testDataFilePath = "src/test/resources/testData/";
+    static ObjectMapper mapper = new ObjectMapper();
     public static Map<String, String> readJsonFile(String fileName) {
-        ObjectMapper mapper = new ObjectMapper();
+
         try {
             File file=new File(testDataFilePath + fileName);
             return mapper.readValue(file,new TypeReference<Map<String, String>>() {});
@@ -18,4 +20,17 @@ public class JsonReader {
         }
 
     }
+
+    public static Map<String, List<Map<String,String>>> readJsonData(String fileName) {
+         try{
+
+             File file = new File(testDataFilePath + fileName);
+             return mapper.readValue(file, new TypeReference<Map<String, List<Map<String, String>>>>() {});
+         }catch(Exception e){
+             throw new RuntimeException("Failed to read test data", e);
+         }
+    }
 }
+
+
+
